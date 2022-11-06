412189a70e7587676b47d09c1b9bc32a79ebeab9
JCaloca <jacobcaloca1@gmail.com>
2022-11-06 12:24:57 -0800

    Created main handlebars structure and public logout js

--- seeds/index.js
+++ seeds/index.js
@@ -8,6 +8,7 @@ const seedAll = async () => {
   // force: true, creates the table dropping it if it already exists
   await sequelize.sync({ force: true });
   console.log("\n--- DATABASE SYNCED ---\n");
+
   // Seeding model tables
   await seedUser();
   console.log("\n--- USER SEEDED ---\n");
